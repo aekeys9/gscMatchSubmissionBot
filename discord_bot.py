63789d0ca725a63049ca5d4c2d906284835eee45
Alex Keys <122501316+aekeys9@users.noreply.github.com>
2023-09-01 12:01:07 -0400

    Added Comments to discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -1,11 +1,20 @@
+"""
+GSC Match Submission Bot - Discord Interface
+A program for users to send data from their matches via a Discord Bot
+Made by: Alex Keys
+"""
+
+#IMPORTS
 import discord as ds
 from discord import app_commands
 from discord.ext import commands
 import MKTBAPI as api
 
+#BOT INFORMATION
 bot = commands.Bot(command_prefix="!", intents=ds.Intents.all())
-token = "Hidden for your and my safety."
+token = "Hidden for my safety."
 
+#BOT INITIALIZATION
 @bot.event
 async def on_ready():
     print('Bot is Live.')
@@ -15,7 +24,7 @@ async def on_ready():
     except:
         print("error with sync")    
 
-
+#'Submit' COMMAND: players enter in their Table Bot ID & their GSC Division and the data from the ID gets stored into .txts 
 @bot.tree.command(name="submit")
 @app_commands.describe(table_id = "String of #s sent when table is first created.")
 @app_commands.describe(division = "Divison you are currently playing for.")
@@ -23,5 +32,6 @@ async def submit(interaction: ds.Interaction, table_id: str, division: str):
     await interaction.response.send_message("Submitting... Please wait a moment.", delete_after=5)
     api.main(table_id, division) #function ran
     await interaction.followup.send(content=f"Submitted **Divison {division}** match with ID **{table_id}** to the Analytics Team!",)
-    
+
+#TURNING ON THE BOT
 bot.run(token)
